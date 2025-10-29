9d333247165da0efc0446da40f021e2421201ab5
MaxSmolenskiy <ivanmax97@gmail.com>
2025-10-29 21:35:49 +0300

    улучшили чего то

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,5 +4,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("11111111111");
+        System.out.println("------");
     }
 }
